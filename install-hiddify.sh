e7fc177f371c6cea58b3630416969d9c1de81bab
Sergey <100784232+SNeverov@users.noreply.github.com>
2025-04-04 20:23:21 +0500

    Update install-hiddify.sh

--- install-hiddify.sh
+++ install-hiddify.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-echo "🛠️ Обновляем систему и устанавливаем зависимости..."
+echo "Обновляем систему и устанавливаем зависимости..."
 apt update && apt install -y curl sudo gnupg apt-transport-https ca-certificates software-properties-common
 
-echo "🐳 Устанавливаем Docker..."
+echo "Устанавливаем Docker..."
 curl -fsSL https://get.docker.com | sh
 systemctl enable docker
 systemctl start docker
 
-echo "📦 Загружаем и запускаем установку Hiddify Next..."
+echo "Загружаем и запускаем установку Hiddify Next..."
 curl -L -o install.sh https://raw.githubusercontent.com/hiddify/hiddify-config/main/install.sh
 chmod +x install.sh
 ./install.sh
